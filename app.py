f4f85181caa5c0eceabda6b7cc050bb40969183a
Alex Pattison <alexlpattison@gmail.com>
2019-02-04 14:45:38 -0800

    Add jwt logic

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, redirect
 import sign
+import jwt
+import os
 
 app = Flask(__name__)
+
+secret = os.environ['SECRET']
 
 
 @app.route('/')
 def pull_url():
-    key = request.args.get('key')
-    generated_url = sign.generate_presigned_url(key)
+    encoded = request.args.get('token')
+    decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
+    generated_url = sign.generate_presigned_url(decoded)
+    print(generated_url)
     return redirect(generated_url)
--- sign.py
+++ sign.py
@@ -1,14 +1,9 @@
 import boto3
-import os
 
-s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
-# Your Bucket Name
-bucket = s3.Bucket(os.environ['BUCKET'])
 
-
-def generate_presigned_url(key):
+def generate_presigned_url(jwt):
     url = s3_client.generate_presigned_url(ClientMethod='get_object', Params={
-        'Bucket': bucket.name, 'Key': key}, ExpiresIn=5)
+        'Bucket': jwt['bucket'], 'Key': jwt['key']}, ExpiresIn=60)
     return url
