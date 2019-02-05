6b56cb79aa3bfffde1c710943063d7a9a934c1f8
Alex Pattison <alexlpattison@gmail.com>
2019-02-04 16:42:36 -0800

    Cleaned up code --working version of deployable flask app

--- app.py
+++ app.py
@@ -11,13 +11,9 @@ secret = os.environ['SECRET']
 @app.route('/')
 def pull_url():
     encoded = request.args.get('token')
-    if not encoded:
-        return 'Please specify a token'
-    else:
-        decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
-        generated_url = sign.generate_presigned_url(decoded)
-        print(generated_url)
-        return redirect(generated_url)
+    decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
+    generated_url = sign.generate_presigned_url(decoded)
+    return redirect(generated_url)
 
 
 if __name__ == '__main__':
