be695d9076e2269aeda9d8cff8e7cec88c3a02a5
Alex Pattison <alexlpattison@gmail.com>
2019-02-04 16:17:29 -0800

    local deploy working

--- app.py
+++ app.py
@@ -11,7 +11,14 @@ secret = os.environ['SECRET']
 @app.route('/')
 def pull_url():
     encoded = request.args.get('token')
-    decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
-    generated_url = sign.generate_presigned_url(decoded)
-    print(generated_url)
-    return 'Hello World'
+    if not encoded:
+        return 'Please specify a token'
+    else:
+        decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
+        generated_url = sign.generate_presigned_url(decoded)
+        print(generated_url)
+        return redirect(generated_url)
+
+
+if __name__ == '__main__':
+    app.run()
